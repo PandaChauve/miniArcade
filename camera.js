89289d6099a64dcf2587f34c2e0020038a378055
PandaChauve <sylvain.chevremont@gmail.com>
2017-09-03 15:27:28 +0200

    physics

--- camera.js
+++ camera.js
@@ -14,7 +14,7 @@ function Camera(width, height, target, world)
     this.container = new PIXI.Container();
     if(world)
     {
-        this.container.scale.set(width/world.width, height/world.height);
+        this.container.scale.set(width/world.width,height/world.height);
         this.target = null;//never target in minimap mode
     }
 }
@@ -31,8 +31,11 @@ Camera.prototype.draw = function(world)
             {
                 obj.cam[this.camId] = spriteCache.createSprite(obj.name);
             }
-            obj.cam[this.camId].position.set(obj.x, obj.y);
-            obj.cam[this.camId].rotation = obj.rotation;
+            if(obj.body)
+                obj.cam[this.camId].position.set(obj.body.position[0], -obj.body.position[1]);
+            else
+                obj.cam[this.camId].position.set(obj.x, obj.y);
+            obj.cam[this.camId].rotation = -obj.body.angle;
             this.container.addChild(obj.cam[this.camId]);
         }
     }
@@ -41,8 +44,8 @@ Camera.prototype.draw = function(world)
     {
         var leftMargin = this.width/2;
         var topMargin = this.height/2;
-        var x = Math.max(Math.min(-this.target.x, -leftMargin), -world.width+leftMargin);
-        var y = Math.max(Math.min(-this.target.y, -topMargin), -world.height+topMargin);
+        var x = Math.max(Math.min(-this.target.body.position[0], -leftMargin), -world.width+leftMargin);
+        var y = Math.max(Math.min(this.target.body.position[1], -topMargin), -world.height+topMargin);
 
         this.container.position.set(x+leftMargin,y+topMargin);
     }
